e38900103d2efc7f74ba0bc6931be0ef5d36373f
DanielCornelio <jdcornelio89@gmail.com>
2020-03-04 14:16:44 -0600

    variables

--- ejer02-variables/main.go
+++ ejer02-variables/main.go
@@ -4,16 +4,22 @@ import "fmt"
 
 var numero int
 var texto string
-var status bool
+var status bool = true
 
 func main() {
-	var numero2, numero3, numero4, texto = 2, 3, 4, "daniel"
+	var numero2, numero3, numero4, texto, status = 2, 3, 4, "daniel", false
 
-	var moneda float64 = 0
-	numero2 = moneda
+	// var moneda float64 = 0
+	// numero2 = moneda
 
 	fmt.Println(numero2)
 	fmt.Println(numero3)
 	fmt.Println(numero4)
 	fmt.Println(texto)
+	fmt.Println(status)
+
+	mostrarStatus()
+}
+func mostrarStatus() {
+	fmt.Println(status)
 }
