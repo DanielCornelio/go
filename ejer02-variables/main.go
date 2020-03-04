23dd4736e7c5899c7046947fec95ef408acdc211
DanielCornelio <jdcornelio89@gmail.com>
2020-03-04 14:18:13 -0600

    variables

--- ejer02-variables/main.go
+++ ejer02-variables/main.go
@@ -9,8 +9,8 @@ var status bool = true
 func main() {
 	var numero2, numero3, numero4, texto, status = 2, 3, 4, "daniel", false
 
-	// var moneda float64 = 0
-	// numero2 = moneda
+	var moneda float64 = 0
+	numero2 = int(moneda)
 
 	fmt.Println(numero2)
 	fmt.Println(numero3)
